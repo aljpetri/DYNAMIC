2b35f145a57edb2be226d9584ac9fedbd5d2eb76
AlPetri <alpetri@students.uni-mainz.de>
2020-06-16 18:27:40 +0300

    Fixed suc error with help from Massimiliano Rossi

--- B_tree_operations.h
+++ B_tree_operations.h
@@ -22,14 +22,14 @@ using namespace md;
  * @param minimizerTree:    the B-tree to be printed
  */
 void print_minimizerTree(B_tree<int,std::string,7,3>* minimizerTree){
-  int i = 0;
+  //int i = 0;
   for(auto elem: *minimizerTree){
     int minikey=elem.first;
     auto elem3 = minimizerTree->search(minikey);
     std::vector<std::string> es2=*elem3.key->satellites;
     std::string sequence=es2[0];
     cout<<"Minimizer at "<<elem.first<<":" <<sequence<<"\n";
-    i+= 1;
+    //i+= 1;
   }
 }
 /*!
@@ -55,6 +55,9 @@ void delete_minimizers_inefficient(B_tree<int,std::string,7,3>* minimizerTree,in
   for(int i = left; i <=right; i+=1){
     cout<<"Removing "<<i<<" \n";
     minimizerTree->remove(i);
+    if(!minimizerTree->is_empty()){
+    minimizerTree->remove(i);
+    }
   }
 }
 
@@ -68,7 +71,7 @@ void delete_minimizers_inefficient(B_tree<int,std::string,7,3>* minimizerTree,in
  * @param var_impact_shift: the length by which subsequent minimizers key have to be shifted
  */
 void update_minimizerTree(B_tree<int,std::string,7,3>* minimizerTree,std::string& fullsubseq,int& thisstartpos,int& k_size,int& w_size, int& var_impact_shift){
-//generate the minimizers for the updated subsequence
+  //generate the minimizers for the updated subsequence
   std::vector<Minimizer> newminis=get_kmer_minimizers_algo(fullsubseq, k_size, w_size,thisstartpos);
   //find the positions of the first and last minimizer in the new set
   int start=newminis.front().getPosition();
@@ -79,8 +82,8 @@ void update_minimizerTree(B_tree<int,std::string,7,3>* minimizerTree,std::string
   //find the last minimizer position in the minimizer tree
   int lastminipos=minimizerTree->get_max();
 
-cout<<"Shifting the elements by "<<var_impact_shift<<"\n";
-cout<<"Newend "<<newend<<", Lastminipos: "<<lastminipos<<"\n";
+  //cout<<"Shifting the elements by "<<var_impact_shift<<"\n";
+  cout<<"Newend "<<newend<<", Lastminipos: "<<lastminipos<<"\n";
 
 //as the B-tree does not deliver stop if after last element set position of last deleted minimizer to maximum in tree
   int suc=0;
@@ -90,17 +93,15 @@ cout<<"Newend "<<newend<<", Lastminipos: "<<lastminipos<<"\n";
   }*/
   if(newend>=lastminipos){
     newend=lastminipos;
-    suc=0;
   }
   else{
     cout<<"I want to find "<<newend<<" in the tree \n";
     //int suc=minimizerTree->search(newend).key->value;
     //auto succ=minimizerTree->successor(newend).key->value;
-    int suc=minimizerTree->successor(newend).key->value;
+    suc=minimizerTree->successor(newend).shift_key().value;
     //auto suc=minimizerTree->predecessor(succ).key->value;
     cout<<"found suc "<<suc<<"\n";
   }
-  cout<<"Hello World\n";
   cout<<"deleting the minimizers in the interval ("<<start<<", "<<newend<<")\n";
   //cout<<"deleting the minimizers in the interval ("<<start<<", "<<newend<<")\n";
   cout<<"last element in minimizerTree "<<lastminipos<<"\n";
@@ -111,20 +112,35 @@ cout<<"Newend "<<newend<<", Lastminipos: "<<lastminipos<<"\n";
 
   //int right=minimizerTree->search(end).key->value;
   //delete all minimizers between left and right
-if(!(start>minimizerTree->get_max())){
+  //delete all minimizers which are affected by the variation
+  if(!(start>minimizerTree->get_max())){
   delete_minimizers_inefficient(minimizerTree,start,newend);
   //delete_minimizers_iterator(minimizerTree,start,newend);
-}
-  if(suc>0){
-    minimizerTree->shift_greater(suc,var_impact_shift);
   }
-  print_minimizerTree(minimizerTree);
+  if(!minimizerTree->is_empty()){
+    cout<<"B-tree is not empty\n";
+    //shift all minimizers located at a position greater/equal than suc
+    if(suc){
+      cout<<"Shifting all minimizers greater than "<<suc<<" by "<<var_impact_shift<<"\n";
+      minimizerTree->shift_greater(suc,var_impact_shift);
+    }
+    cout<<"minimizer tree after applying shift: \n";
+    print_minimizerTree(minimizerTree);
+    cout<<"after applying shift done: \n";
+
+  }
+  else{
+    cout<<"B-tree is empty\n";
+    //minimizerTree->~B_tree();
+    //B_tree<int,std::string,7,3>* minimizerTree = new B_tree<int,std::string,7,3>();
+  }
   cout<<"New Minimizers to be added:\n";
   for(int i=0;i<newminis.size();i++){
     newminis[i].printMinimizer();
   }
   cout<<"printing new minimizers done\n";
   fill_minimizer_tree(minimizerTree,newminis);
+  cout<<"New Minimizer Tree:\n";
   print_minimizerTree(minimizerTree);
   cout<<"Updating done\n";
 }
@@ -139,7 +155,7 @@ std::vector<Minimizer> minimizer_to_vector(B_tree<int,std::string,7,3>* minimize
     int minikey=elem.first;
     auto elem3 = minimizerTree->search(minikey);
     std::vector<std::string> es2=*elem3.key->satellites;
-    std::string sequence=es2[0];
+    std::string sequence=es2.back();
     Minimizer mini=Minimizer(minikey,sequence);
     minimizers.push_back(mini);
   }
